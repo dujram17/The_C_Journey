3ace41910960e05df4eecbb06297ebdb760b1d93
dujram17 <dujramsahu883@gmail.com>
2026-08-15 00:22:33 +0530

    Program for Insertion Sort

---
+++ insertion_sort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void print(int *arr,int n){
+    for(int i = 0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
+void insertionsort(int *arr,int n){
+    for(int i = 1;i<n;i++){
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]>curr){
+          arr[prev+1] = arr[prev];
+          prev--;
+
+        }
+        arr[prev+1] = curr;
+    }
+    print(arr,n);
+}
+int main(){
+    int arr[] = {5,4,3,6,1,2};
+    int n = sizeof(arr)/sizeof(int);
+    insertionsort(arr,n);
+    return 0;
+}
